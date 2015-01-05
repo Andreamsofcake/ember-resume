9514a5f01c94aa033f43cf422cb7bb80ddaa4744
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-01-04 22:33:37 -0800

    creating other routes

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['Passionate about writing quality code', 'Excited about making a difference as part of a team', 'Fast learner even faster innovator'];
+  }
+});
+
+
+App.PortfolioRoute = Ember.Route.extend({
+  model: function() {
+    return ['more here later'];
   }
 });
