d36e2e4a6df2f6f14fc23048b4093470de283bde
“Andreamsofcake” <andrea.moulding@gmail.com>
2015-02-28 12:18:09 -0800

    updating jquery script

--- js/app.js
+++ js/app.js
@@ -12,3 +12,17 @@ App.IndexRoute = Ember.Route.extend({
     return ['Passionate about writing quality code', 'Excited about making a difference as part of a team', 'Fast learner even faster innovator'];
   }
 });
+
+// App.BlogRoute = Ember.Route.extend({
+//   model: function handleResponse(response) {
+//                       console.log(response);
+//                       var posts = response.items;
+//                       posts.forEach(function(post){
+//                         var date = post.published;
+//                       document.getElementById("content")
+//                       .innerHTML += "<h3>" + post.title 
+//                       + "</h3>" + "<h4>" + "Published" + " " + moment(date).format('MMM DD YYYY h:mmA') + "<h4>" 
+//                       + "<p>" + post.content + "</p>";
+//                       });
+//                     }
+// });
